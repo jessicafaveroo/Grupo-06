806f5224549efc19b80ef4e49baa25c2fa51a9d6
Samuel Modesto <samuelmodestoes@gmail.com>
2020-10-07 19:56:26 -0300

    fix(regras):RPIII-21 Correção  nomenclaturas dos argumentos das regras

--- Entrega 2/regras.pl
+++ Entrega 2/regras.pl
@@ -1,26 +1,26 @@
 %% Regras
 
-ministra(X,Y,H):-
-        professor_preferencia(X,Y,1),
-        disciplina_semestre(Y, 1); disciplina_semestre(Y, 3), %%Regra 6
-				disc_mesmo_horario :- disciplina_semestre(Y, 1), disciplina_semestre(Y, 3) :- horario(U, H) < 1. %%Regra 2
+ministra(PROFESSOR, MATERIA, H):-
+        professor_preferencia(PROFESSOR, MATERIA, 1),
+        disciplina_semestre(MATERIA, 1); disciplina_semestre(MATERIA, 3), %%Regra 6
+				disc_mesmo_horario :- disciplina_semestre(MATERIA, 1), disciplina_semestre(MATERIA, 3) :- horario(U, H) < 1. %%Regra 2
 
-ministra(X,Y,H):-
-        professor_preferencia(X,Y,2),
-        disciplina_semestre(Y, 1); disciplina_semestre(Y, 3), %%Regra 6
-				disc_mesmo_horario :- disciplina_semestre(Y, 1), disciplina_semestre(Y, 3) :- horario(U, H) < 1. %%Regra 2
+ministra(PROFESSOR, MATERIA, H):-
+        professor_preferencia(PROFESSOR, MATERIA, 2),
+        disciplina_semestre(MATERIA, 1); disciplina_semestre(Y, 3), %%Regra 6
+				disc_mesmo_horario :- disciplina_semestre(MATERIA, 1), disciplina_semestre(MATERIA, 3) :- horario(U, H) < 1. %%Regra 2
 
-ministra(X,Y,H):-
-        professor_preferencia(X,Y,3),
-        disciplina_semestre(Y, 1); disciplina_semestre(Y, 3), %%Regra 6
-				disc_mesmo_horario :- disciplina_semestre(Y, 1), disciplina_semestre(Y, 3) :- horario(U, H) < 1. %%Regra 2
+ministra(PROFESSOR, MATERIA, H):-
+        professor_preferencia(PROFESSOR, MATERIA, 3),
+        disciplina_semestre(MATERIA, 1); disciplina_semestre(MATERIA, 3), %%Regra 6
+				disc_mesmo_horario :- disciplina_semestre(MATERIA, 1), disciplina_semestre(MATERIA, 3) :- horario(U, H) < 1. %%Regra 2
 
-ministra(X,Y,H):-
-        professor_preferencia(X,Y,4),
-        disciplina_semestre(Y, 1); disciplina_semestre(Y, 3), %%Regra 6
-				disc_mesmo_horario :- disciplina_semestre(Y, 1), disciplina_semestre(Y, 3) :- horario(U, H) < 1. %%Regra 2
+ministra(PROFESSOR, MATERIA, H):-
+        professor_preferencia(PROFESSOR, MATERIA, 4),
+        disciplina_semestre(MATERIA, 1); disciplina_semestre(MATERIA, 3), %%Regra 6
+				disc_mesmo_horario :- disciplina_semestre(MATERIA, 1), disciplina_semestre(MATERIA, 3) :- horario(U, H) < 1. %%Regra 2
 
-ministra(X,Y,H):-
-        professor_preferencia(X,Y,5),
-        disciplina_semestre(Y, 1); disciplina_semestre(Y, 3), %%Regra 6
-				disc_mesmo_horario :- disciplina_semestre(Y, 1), disciplina_semestre(Y, 3) :- horario(U, H) < 1. %%Regra 2
+ministra(PROFESSOR, MATERIA, H):-
+        professor_preferencia(PROFESSOR, MATERIA, 5),
+        disciplina_semestre(MATERIA, 1); disciplina_semestre(MATERIA, 3), %%Regra 6
+				disc_mesmo_horario :- disciplina_semestre(MATERIA, 1), disciplina_semestre(MATERIA, 3) :- horario(U, H) < 1. %%Regra 2
